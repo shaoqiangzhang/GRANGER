e4f1e21b25ab10ee8ac436cdbc5bbb7def1df8d0
Shaoqiang ZHANG <35386642+shaoqiangzhang@users.noreply.github.com>
2024-10-23 15:35:44 +0800

    Add files via upload

---
+++ timepseudo.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from proprocess import get_origin_expression_data
+
+def time_rank(expression_path,PseudoTime_path):
+    expression_df = pd.read_csv(expression_path, index_col=0)
+    pseudotime_df = pd.read_csv(PseudoTime_path, index_col=0)
+    # pseudotime_df['valid_time'] = pseudotime_df['PseudoTime'] 
+    pseudotime_df['valid_time'] = pseudotime_df['PseudoTime1'].fillna(pseudotime_df['PseudoTime2'])
+    # pseudotime_df['valid_time'] = pseudotime_df['PseudoTime1'].fillna(pseudotime_df['PseudoTime2']).fillna(pseudotime_df['PseudoTime3']).fillna(pseudotime_df['PseudoTime4'])
+    sorted_pseudotime_df = pseudotime_df.sort_values(by='valid_time')
+    sorted_cellids = sorted_pseudotime_df.index.tolist()
+    sorted_expression_df = expression_df[sorted_cellids]
+    sorted_expression_df.to_csv('time.csv')
+    return sorted_expression_df
+
+
+time_rank('ExpressionData.csv',
+          'PseudoTime.csv')
+
+def to_npy(expression_path):
+    a,b=get_origin_expression_data(expression_path)
+    arr = np.vstack(list(a.values()))
+    np.save('time_output.npy', arr)
+to_npy('time.csv')
